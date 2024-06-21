a2b78a7642be9a18fa209e64fe33b1f23fe1e19e
miyasic <kou.sepak@gmail.com>
2024-06-22 08:01:04 +0900

    functionsからgeminiapiを叩けるようにした

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -1,9 +1,11 @@
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 from summarize import summarize_text
+from firebase_functions.params import SecretParam
 
 initialize_app()
 
+API_KEY = SecretParam('API_KEY')
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
@@ -13,8 +15,9 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
 def on_request_example2(req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response("Hello world2!")
 
-@https_fn.on_request()
+@https_fn.on_request(secrets=[API_KEY])
 def summarize(req: https_fn.Request) -> https_fn.Response:
     url = req.json['url']
-    content = summarize_text(url)
+    api_key = API_KEY.value
+    content = summarize_text(url,api_key)
     return content
--- firebase/functions/summarize.py
+++ firebase/functions/summarize.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import google.generativeai as genai
+import re
+import json
 
-
-def summarize_text(url):
+def summarize_text(url, api_key):
     content = fetch_article_content(url)
-    return content
+    response_text = request_gemini("次の内容を50文字×3の箇条書きで読者が興味を持つように要約してください。title,summary1~3をkeyとしたjsonで返してください。" + content, api_key)
+    extracted_data = extract_json_from_markdown(response_text)
+    print(extracted_data['title'])
+    print(extracted_data['summary1'])
+    print(extracted_data['summary2'])
+    print(extracted_data['summary3'])
+
+    json_data = json.dumps(extracted_data, ensure_ascii=False, indent=2)
+    return json_data
 
 
 def fetch_article_content(url):
@@ -16,3 +26,18 @@ def fetch_article_content(url):
     paragraphs = soup.find_all('p')
     content = ' '.join([p.get_text() for p in paragraphs])
     return content
+
+def request_gemini(text, api_key):
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(text)
+    return response.text
+
+def extract_json_from_markdown(markdown):
+    # 正規表現パターンを定義します
+    pattern = r'\"(title|summary1|summary2|summary3)\":\s*\"([^\"]*)\"'
+    
+    # 正規表現でマッチする部分を抽出し、辞書に格納します
+    matches = re.findall(pattern, markdown)
+    extracted_data = {key: value for key, value in matches}
+    return extracted_data
