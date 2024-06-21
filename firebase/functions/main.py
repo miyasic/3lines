41bf3b69eee56400f457d7456879f10eb56d78cd
miyasic <kou.sepak@gmail.com>
2024-06-22 06:27:32 +0900

    hello world 2

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -11,3 +11,7 @@ initialize_app()
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response("Hello world!")
+
+@https_fn.on_request()
+def on_request_example2(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world2!")
