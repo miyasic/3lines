7713f70ce5e220243fe8a0aa5408445bdfbdf440
miyasic <kou.sepak@gmail.com>
2024-06-22 12:18:40 +0900

    背景画像を適用

--- firebase/functions/generate_summarized_image.py
+++ firebase/functions/generate_summarized_image.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 import uuid
 
 FONT_PATH = "assets/Noto_Sans_JP/static/NotoSansJP-ExtraBold.ttf"
+BACKGROUND_IMAGE_PATH = "assets/image/default_background.png"
 TITLE_FONT_SIZE = 32
 NORMAL_FONT_SIZE = 24
 
@@ -9,16 +10,17 @@ IMAGE_WIDTH, IMAGE_HEIGHT = 1200, 628
 
 def generate_image(title, summary1, summary2, summary3):
     # 背景画像を生成または読み込み
-    image = Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), color=(73, 109, 137))
+    image = Image.open(BACKGROUND_IMAGE_PATH).resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+    # image = Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), color=(73, 109, 137))
     draw = ImageDraw.Draw(image)
     font_title = ImageFont.truetype(FONT_PATH, TITLE_FONT_SIZE)
     font_normal = ImageFont.truetype(FONT_PATH, NORMAL_FONT_SIZE)
 
     # 文字を画像に描画
-    draw.text((50, 50), title, font=font_title, fill=(255, 255, 255))
-    draw.text((50, 150), "・　summary1", font=font_normal, fill=(255, 255, 255))
-    draw.text((50, 250), "・　summary2", font=font_normal, fill=(255, 255, 255))
-    draw.text((50, 350), "・　summary3", font=font_normal, fill=(255, 255, 255))
+    draw.text((50, 50), title, font=font_title, fill=(0,0,0))
+    draw.text((50, 150), f"・　{summary1}", font=font_normal, fill=(0,0,0))
+    draw.text((50, 250), f"・　{summary2}", font=font_normal, fill=(0,0,0))
+    draw.text((50, 350), f"・　{summary3}", font=font_normal, fill=(0,0,0))
 
     # 一時ファイルに保存
     file_path = f'/tmp/{uuid.uuid4()}.png'
