858eee57e37cd09c3b9935cbf0482668440058ae
miyasic <kou.sepak@gmail.com>
2024-06-24 21:55:11 +0900

    firestoreから記事情報を取得して表示

---
+++ front/src/app/firebase/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDl0jNRpeZySiT7HPFAtndU-F8CIkPqNwY",
+    authDomain: "lines-31c04.firebaseapp.com",
+    projectId: "lines-31c04",
+    storageBucket: "lines-31c04.appspot.com",
+    messagingSenderId: "856500639204",
+    appId: "1:856500639204:web:2f9eb48e1e03360c98e3f1",
+    measurementId: "G-R3KEXWL32W"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const firestore = firebase.firestore();
+
+export { firestore };
+export default firebase;
