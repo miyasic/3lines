7c298538b5e85dce490c6cbb5c66b976b16ab6a3
miyasic <kou.sepak@gmail.com>
2024-07-06 23:06:49 +0900

    ImagePreviewのロジックを分離

--- front/src/components/IMagePreview.tsx
+++ front/src/components/IMagePreview.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import CommonLayout from '@/components/summary/CommonLayout';
 import styles from './ImagePreview.module.css';
+import useEditableSummary from './useEditableSummary';
 
-const MAX_CHARS = 30;
 
 type EditableSummaryProps = {
     summary: string;
@@ -51,34 +51,7 @@ const ImagePreview: React.FC<ImagePreviewProps> = ({
     setSummary3,
     style
 }) => {
-    const [isOverLimit, setIsOverLimit] = useState({
-        title: false,
-        summary1: false,
-        summary2: false,
-        summary3: false
-    });
-
-    const handleInput = useCallback((setter: (value: string) => void, field: keyof typeof isOverLimit) => (event: React.FormEvent<HTMLDivElement>) => {
-        const newText = event.currentTarget.textContent || "";
-        if (newText.length <= MAX_CHARS) {
-            setter(newText);
-            setIsOverLimit(prev => ({ ...prev, [field]: false }));
-        } else {
-            event.currentTarget.textContent = newText.slice(0, MAX_CHARS);
-            setter(newText.slice(0, MAX_CHARS));
-            setIsOverLimit(prev => ({ ...prev, [field]: true }));
-        }
-    }, []);
-
-    const handlePaste = useCallback((setter: (value: string) => void, field: keyof typeof isOverLimit) => (event: React.ClipboardEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        const pastedText = event.clipboardData.getData('text');
-        const currentText = event.currentTarget.textContent || "";
-        const newText = (currentText + pastedText).slice(0, MAX_CHARS);
-        event.currentTarget.textContent = newText;
-        setter(newText);
-        setIsOverLimit(prev => ({ ...prev, [field]: newText.length > MAX_CHARS }));
-    }, []);
+    const { isOverLimit, handleInput, handlePaste } = useEditableSummary();
 
     return (
         <CommonLayout
