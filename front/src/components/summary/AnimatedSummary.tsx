225b37732e16df7c7a7896e11fbf05fa8a528f73
miyasic <kou.sepak@gmail.com>
2024-07-06 22:04:12 +0900

    アニメーションの速度を定数宣言

--- front/src/components/summary/AnimatedSummary.tsx
+++ front/src/components/summary/AnimatedSummary.tsx
@@ -9,6 +9,8 @@ type AnimatedSummaryProps = {
     summary3: string;
     backgroundImage: string;
 };
+// 1文字あたりの表示速度を指定するspeedプロパティを追加
+const SPEED_PER_CHAR = 25;
 
 const AnimatedSummary: React.FC<AnimatedSummaryProps> = ({ title, summary1, summary2, summary3, backgroundImage }) => {
     const [showSummary1, setShowSummary1] = useState(false);
@@ -18,9 +20,9 @@ const AnimatedSummary: React.FC<AnimatedSummaryProps> = ({ title, summary1, summ
     return (
         <CommonLayout
             title={title}
-            summary1={<TypingAnimation text={summary1} speed={25} onComplete={() => setShowSummary1(true)} />}
-            summary2={showSummary1 ? <TypingAnimation text={summary2} speed={25} onComplete={() => setShowSummary2(true)} /> : null}
-            summary3={showSummary2 ? <TypingAnimation text={summary3} speed={25} /> : null}
+            summary1={<TypingAnimation text={summary1} speed={SPEED_PER_CHAR} onComplete={() => setShowSummary1(true)} />}
+            summary2={showSummary1 ? <TypingAnimation text={summary2} speed={SPEED_PER_CHAR} onComplete={() => setShowSummary2(true)} /> : null}
+            summary3={showSummary2 ? <TypingAnimation text={summary3} speed={SPEED_PER_CHAR} /> : null}
             backgroundImage={backgroundImage}
         />
     );
