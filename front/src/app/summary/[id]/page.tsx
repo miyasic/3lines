3956e116d1205764147f0833327869f48ba1770e
miyasic <kou.sepak@gmail.com>
2024-07-10 07:25:31 +0900

    urlを変更

--- front/src/app/summary/[id]/page.tsx
+++ front/src/app/summary/[id]/page.tsx
@@ -28,7 +28,7 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
                     url: data.imageUrl,
                 },
             ],
-            url: `https://3lines-afx83u1kq-miyasics-projects.vercel.app/summary/${params.id}`,
+            url: `https://3lines-lemon.vercel.app/summary/${params.id}`,
         },
     };
 }
