7db56f5ba987bd939aba824180b4b3e06c89a77f
miyasic <kou.sepak@gmail.com>
2024-08-07 20:59:15 +0900

    匿名認証時に自分の記事を開いたら、認証促進ダイアログを表示

--- front/src/constants/constantsTexts.ts
+++ front/src/constants/constantsTexts.ts
@@ -25,3 +25,5 @@ export const CONFIRM_DELETE_SUMMARY = `非公開にすると、一覧画面に�
 export const ALREADY_DELETED_SUMMARY = `非公開の要約は一覧画面に表示されません。
 詳細ページへのアクセスは引き続き可能です。`;
 export const THIS_IS_ALL = "これで全部です！";
+export const PROMOTE_GITHUB_SIGNIN_TITLE = "GitHubアカウントで認証をしませんか？";
+export const PROMOTE_GITHUB_SIGNIN_DESCRIPTION = '現在あなたは匿名ユーザーです。匿名ユーザーの作成した要約は24時間後に一覧から削除されます。GitHubアカウントで認証を行うと制限が開放され、マイページから記事を管理できるようになります。';
